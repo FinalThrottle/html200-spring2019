1b3bfddc0c6beb8bd6e81a0722235be12eb1d003
Alex <michyuh@gmail.com>
2019-04-22 20:01:14 -0700

    Added exercise two of lesson 03.

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,10 @@
+
+let maureenWalsh = 'Those nurses probably do get breaks. They probably play cards for a considerable amount of the day.';
+
+// Make an array from that sentence.
+
+let quote = maureenWalsh.split(' ');
+
+// And console.log the third word.
+
+console.log(quote[2]);
