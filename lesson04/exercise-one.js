7aff5c88c2f749a1d16e47ce56507ac0dca3185f
Alex <michyuh@gmail.com>
2019-05-06 17:54:02 -0700

    Completed exercise one and two.

---
+++ lesson04/exercise-one.js
@@ -0,0 +1,10 @@
+function squareInputNumber() {
+
+  let input = prompt("Square a number");
+
+  function squaredNumber(number) {
+    return number * number;
+  }
+
+  alert(squaredNumber(input));
+}
